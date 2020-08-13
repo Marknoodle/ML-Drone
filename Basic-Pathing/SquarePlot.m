93bf778dd1e662fd5bd61390ccdc0dbf153b8c17
Mark Holcomb <markholcomb5@gmail.com>
2020-08-12 21:10:19 -0400

    Refactored entire functionality

--- Basic-Pathing/SquarePlot.m
+++ Basic-Pathing/SquarePlot.m
@@ -11,72 +11,26 @@ ylabel('y-axis')
    
    edgeIndex = 0; %Which edge of the 'square' we are on
    distanceL = 0.5; %Meters
-   distanceS = 0.25;
+
    speed = 0.5; %Meters/Sec
-   tObj = tic;
    
    drone = animatedline('LineWidth',2,"color","r");
    
    leftoff = zeros(1:2);
    
-  % totalDistance = distance
-   speedz = 0;
+  % totalDistance = distanceL * 4 in this case
    while(edgeIndex <= 3)
-       % Move the drone unblocking the command line
        tplot = tic;
-       if (edgeIndex == 0)
-            moveforward(r, 'Distance', distanceL, 'Speed', speed, 'WaitUntilDone', false);
-            timey = tic; %timer to keep track of how long since 'moveforward()'
-            [speedz,timen] = readSpeed(r);
-            % Plot orientation while drone is moving
-            while() %determins if we have gone full mf distance
-                [speedz,timen] = readSpeed(r);
-                tStamp = toc(timey);
-                addpoints(drone, (leftoff(1) + tStamp*speedz(1)),(leftoff(2) + tStamp*speedz(2)) );
-                drawnow;
-            end
-            leftoff(1) = leftoff(1) + distanceL;
-       elseif (edgeIndex == 1)
-            moveright(r, 'Distance', distanceS, 'Speed', speed, 'WaitUntilDone', false);
-            timey = tic; %timer to keep track of how long since 'moveforward()'
-            % Plot orientation while drone is moving
-            while(readSpeed(r) > 0) %determins if we have gone full mf distance
-                [speedz,timen] = readSpeed(r);
-                tStamp = toc(timey);
-                addpoints(drone, (leftoff(1) + tStamp*speedz(1)), (leftoff(2) + tStamp*speedz(2)) );
-                drawnow;
-            end
-            leftoff(2) = leftoff(2) + distanceS;
-       elseif (edgeIndex == 2)
-            moveback(r, 'Distance', distanceL, 'Speed', speed, 'WaitUntilDone', false);
-            timey = tic; %timer to keep track of how long since 'moveforward()'
-            % Plot orientation while drone is moving
-            while(readSpeed(r) > 0) %determins if we have gone full mf distance
-                [speedz,timen] = readSpeed(r);
-                tStamp = toc(timey);
-                addpoints(drone, (leftoff(1) + tStamp*speedz(1)),(leftoff(2) + tStamp*speedz(2)) );
-                drawnow;
-            end
-            leftoff(1) = leftoff(1) - distanceL;
-       elseif (edgeIndex == 3)
-            moveleft(r, 'Distance', distanceS, 'Speed', speed, 'WaitUntilDone', false);
-            timey = tic; %timer to keep track of how long since 'moveforward()'
-            % Plot orientation while drone is moving
-            while(readSpeed(r) > 0) %determins if we have gone full mf distance
-                [speedz,timen] = readSpeed(r);
-                tStamp = toc(timey);
-                addpoints(drone, (leftoff(1) + tStamp*speedz(1)),(leftoff(2) + tStamp*speedz(2)) );
-                drawnow;
-            end
-            leftoff(2) = leftoff(2) - distanceS;
+       switch edgeIndex
+           case 0
+               leftoff = moveWithPlotting(r, "forward", distanceL, speed, drone, leftoff);
+           case 1
+               leftoff = moveWithPlotting(r, "right", distanceL, speed, drone, leftoff);
+           case 2
+               leftoff = moveWithPlotting(r, "back", distanceL, speed, drone, leftoff);
+           case 3
+               leftoff = moveWithPlotting(r, "left", distanceL, speed, drone, leftoff);
        end
-        
-      % Turn the drone after it has traversed one side of the square path
-      clear timey;
-      clear speedz;
-      clear timen;
-      clear tstamp;
-      
       
       pause(5);
       %turn(r, pi/2);
@@ -86,3 +40,53 @@ ylabel('y-axis')
    
       land(r);
       clear;
+
+function position = moveWithPlotting(Drone, Direction, Distance, Speed, AnimatedLine, Position)
+
+    switch Direction
+        case "forward"
+            moveforward(Drone, 'Distance', Distance, 'Speed', Speed, 'WaitUntilDone', false);
+            axis = "x";
+        case "back"
+            moveback(Drone, 'Distance', Distance, 'Speed', Speed, 'WaitUntilDone', false);
+            axis = "x";
+        case "right"
+            moveright(Drone, 'Distance', Distance, 'Speed', Speed, 'WaitUntilDone', false);
+            axis = "y";
+        case "left"
+            moveleft(Drone, 'Distance', Distance, 'Speed', Speed, 'WaitUntilDone', false);
+            axis = "y";
+    end
+    stopWatch = tic;
+    position = plotPathing(Drone, AnimatedLine, axis, stopWatch, Position)
+end
+
+
+function position = plotPathing(Drone, AnimatedLine, Axis, TimeKeeper, Position)
+    pause(0.1)
+    positionMemory = Position;
+    [speed, ~] = readSpeed(Drone);
+    speedInX = speed(1);
+    speedInY = speed(2);
+    switch Axis
+        case "x"
+            while (speedInX > 0)
+                [speed, ~] = readSpeed(Drone);
+                speedInX = speed(1);
+                speedInY = speed(2);
+                timeStamp = toc(TimeKeeper);
+                addpoints(AnimatedLine, positionMemory(1) + timeStamp * speedInX, positionMemory(2) + timeStamp * speedInY);
+                drawnow;
+            end
+        case "y"
+            while (speedInY > 0)
+                [speed, ~] = readSpeed(Drone);
+                speedInX = speed(1);
+                speedInY = speed(2);
+                timeStamp = toc(TimeKeeper);
+                addpoints(AnimatedLine, positionMemory(1) + timeStamp * speedInX, positionMemory(2) + timeStamp * speedInY);
+                drawnow;
+            end
+    end
+    position = [timeStamp * speedInX, timeStamp * speedInY];
+end
