7e7f2103b4d821509bca6b59153408b44dfc41e6
Patrick <patrickwoolard48@gmail.com>
2020-09-16 22:02:37 -0400

    The approach that doesn't work

--- Image Pathing/MatchCard.m
+++ Image Pathing/MatchCard.m
@@ -26,8 +26,13 @@ for i=1:25
     rectangle('Position',[5*scale 5*scale 10*scale 10*scale],'Curvature',1,'EdgeColor','g');
 end
 %% Compare to video frame
-image = imread('fpic.jpg'); % MWsample_full.png
-I = rgb2gray(image);
+vid=VideoReader('bub_vid.mp4');% video we a ripping frames from (vid must be in directory)
+
+n = vid.NumberOfFrames;
+for i = 1:5:n
+
+image = read(vid,i);
+I = rgb2gray(image);%pic has to be gray for SURF
 
 % Detect features
 I_pts = detectSURFFeatures(I);
@@ -54,21 +59,25 @@ title('Showing all matches');
 % I_inlier_pts = I_matched_pts(inlierIdx,:);
 % 
 
+if(length(I_matched_pts) ~= 0)
 [tform,I_inlier_pts,ref_inlier_pts] = estimateGeometricTransform( I_matched_pts,ref_matched_pts,'affine');
 
 % Draw the lines to matched points
 figure;showMatchedFeatures(image, ref_img, I_inlier_pts, ref_inlier_pts, 'montage');
 title('Showing match only with Inliers');
+end 
 
 %% Transform the corner points 
 % This will show where the object is located in the image
 
 %tform = maketform('affine',double(tform_matrix)); %What was originally in MatchCard.m
 
-tform_struct = maketform('affine',double(tform.T));
-
-[width, height,~] = size(ref_img);
-corners = [0,0;height,0;height,width;0,width];
-new_corners = tformfwd(tform_struct, corners(:,1),corners(:,2));
-figure;imshow(image);
-patch(new_corners(:,1),new_corners(:,2),[0 1 0],'FaceAlpha',0.5);
+% tform_struct = maketform('affine',double(tform.T));
+% 
+% [width, height,~] = size(ref_img);
+% corners = [0,0;height,0;height,width;0,width];
+% new_corners = tformfwd(tform_struct, corners(:,1),corners(:,2));
+% figure;imshow(image);
+% patch(new_corners(:,1),new_corners(:,2),[0 1 0],'FaceAlpha',0.5);
+clear I I_features I_inlier_pts I_matched_pts I_pts I_validPts index_pairs ref_inlier_pts ref_matched_pts tform
+end
